5077b024365aeaeddd7125cc8c7bcde9d6b9a3f8
Tom Gao <zimingg2001@gmail.com>
2023-12-18 10:35:49 -0500

    Finally got reactflow to work on website

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,6 +4,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/random', title: 'Random' },
 ]
 
 export default headerNavLinks
